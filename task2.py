9d7d11b24d3c41f406be5dd063ce9c69c116ce21
frankowskimarcin <marcinfrankowski11@gmail.com>
2020-09-02 22:48:34 +0200

    task2 - fixed range of values

--- task2.py
+++ task2.py
@@ -1,16 +1,24 @@
+import random
+
 
 def main():
 
     while True:
+        min_value = random.randint(0, 150)
+        max_value = random.randint(75, 200)
+        if min_value < max_value:
+            break
+
+    while True:
         try:
-            input_value = int(input("Input number in range 50 - 150: "))
+            input_value = int(input("Input number in range {} - {}: ".format(min_value, max_value)))
         except ValueError:
             print("Enter an integer")
             continue
-        if input_value < 50:
-            print("Value must be greater than or equal to 50")
-        elif input_value > 150:
-            print("Value must be less than or equal to 150")
+        if input_value < min_value:
+            print("Value must be greater than or equal to {}".format(min_value))
+        elif input_value > max_value:
+            print("Value must be less than or equal to {}".format(max_value))
         else:
             print("Your value: " + str(input_value))
             break
